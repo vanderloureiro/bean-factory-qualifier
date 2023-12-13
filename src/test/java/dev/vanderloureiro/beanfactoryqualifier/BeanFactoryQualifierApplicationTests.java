444b78e6cf522d37a2d0557861f34cfc1b72e13a
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2023-12-12 21:54:36 -0300

    start project

---
+++ src/test/java/dev/vanderloureiro/beanfactoryqualifier/BeanFactoryQualifierApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vanderloureiro.beanfactoryqualifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanFactoryQualifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
